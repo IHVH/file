9c30d5ee303ddecfad2c4534235855432ddfeb9d
IHVH <ihvh@inbox.ru>
2025-02-22 13:33:38 +0300

    added do_work

--- source.py
+++ source.py
@@ -1,21 +1,40 @@
 import os
 from dataclasses import dataclass
+import json
+import random
 
 @dataclass
 class Point:
      x: int
      y: int
 
+     def __init__(self, x: int, y: int):
+          self.x = x
+          self.y = y
 
-def write_file():
-     filename = r"Download\test.txt"
+_I = 10
+
+def write_file(data: str, i: int):
+
+     filename = fr"Download\{i}-test.json"
      path = os.path.dirname(filename)
      os.makedirs(path, exist_ok=True)
      with open(filename, "w") as f:
-          f.write("test")
+          f.write(data)
     
     
-write_file()
+def do_work():
+     global _I
+     while _I > 0:
+          xr = random.randint(1, 999)
+          yr = random.randint(1, 999)
+          point = Point(xr, yr)
+          json_object = json.dumps(point.__dict__)
+          write_file(json_object, _I)
+          _I -= 1
+
+
+do_work()
 print("OK")
 
 
